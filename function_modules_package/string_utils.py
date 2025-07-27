057eaa35214310073acdccef08ab8534f74fdd4d
mahak6266 <mahaksahare6266@gmail.com>
2025-07-27 23:53:54 +0530

    Add files via upload

---
+++ function_modules_package/string_utils.py
@@ -0,0 +1,6 @@
+from mini_project import ispallindrom, count_the_vowel, frequency_of_letter
+
+name = "bob"
+print(ispallindrom(name))
+print(count_the_vowel(name))
+print(frequency_of_letter(name))
